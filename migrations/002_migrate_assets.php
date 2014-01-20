f629aa24b4bffb7c78cd30acaeace846494784ec
Julien Huriez <syntaxlb@gmail.com>
2014-01-20 11:25:00 +0100

    Fix assets path for migration

--- migrations/002_migrate_assets.php
+++ migrations/002_migrate_assets.php
@@ -6,15 +6,15 @@ class Migrate_assets
 {
 	public function up()
 	{
-            if (is_dir(DOCROOT.'public/assets')) {
+            if (is_dir(DOCROOT.'assets')) {
                 // Create dir
-                is_dir(DOCROOT.'public/assets/js/modules/pricingtable') or mkdir(DOCROOT.'public/assets/js/modules/pricingtable', 0755, TRUE);
-                is_dir(DOCROOT.'public/assets/css/modules/pricingtable') or mkdir(DOCROOT.'public/assets/css/modules/pricingtable', 0755, TRUE);
+                is_dir(DOCROOT.'assets/js/modules/pricingtable') or mkdir(DOCROOT.'assets/js/modules/pricingtable', 0755, TRUE);
+                is_dir(DOCROOT.'assets/css/modules/pricingtable') or mkdir(DOCROOT.'assets/css/modules/pricingtable', 0755, TRUE);
 
                 // Copy JS Assets
-                \File::copy_dir(dirname(__FILE__).'/assets/js', DOCROOT.'public/assets/js/modules/pricingtable');
+                \File::copy_dir(dirname(__FILE__).'/assets/js', DOCROOT.'assets/js/modules/pricingtable');
                 // Copy CSS Assets
-                \File::copy_dir(dirname(__FILE__).'/assets/css', DOCROOT.'public/assets/css/modules/pricingtable');
+                \File::copy_dir(dirname(__FILE__).'/assets/css', DOCROOT.'assets/css/modules/pricingtable');
             } else {
                 die('Unknow public path');
             }
